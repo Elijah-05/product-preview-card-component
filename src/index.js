4d486783a073613dc953ca41cbe185fcaf78b3db
Elijah-05 <125945721+Elijah-05@users.noreply.github.com>
2023-03-30 01:20:28 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import image from './images/image-product-mobile.jpg'
+import cart from './images/icon-cart.svg'
+
+function App() {
+  
+
+  return(
+    <main>
+      <div className='image-container container'>
+        <img src={image} alt='Perfume (Gabrielle Essence Eau De Parfum)'/>
+      </div>
+      <div className='desc-container container'>
+        <p>perfume</p>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>A floral, solar and voluptuous interpretaion composed by Olivier 
+        <br/> Polge, Perfumer-Creator for the House of CHANEL.</p>
+        <p id='price'> <span id='discount-price'>$149.99</span> <span id='original-price'><strike>$169.99</strike></span> </p>
+        <button><img src={cart} alt='cart icon'/> <span> Add to Cart</span></button>
+      </div>
+    </main>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +31,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
